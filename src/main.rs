2340898e755175ddeee9d7a569ea197d883fe6b6
André Puel <andrepuel@gmail.com>
2022-11-14 16:41:39 -0300

    Printing more debug information

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,63 @@
-use std::net::{Ipv6Addr, SocketAddr};
-
+use bytes::Bytes;
 use serde_json::Value;
-use warp::{hyper::HeaderMap, path::FullPath, Filter};
+use std::{
+    net::{Ipv6Addr, SocketAddr},
+    str::FromStr,
+};
+use warp::{
+    hyper::{HeaderMap, Method},
+    path::FullPath,
+    Filter,
+};
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let all = warp::path::full()
+    let all = warp::filters::method::method()
+        .and(warp::path::full())
         .and(warp::header::headers_cloned())
+        .and(warp::query::query())
         .and(
-            warp::body::json()
-                .map(Some)
+            warp::body::bytes()
+                .map(|bytes: Bytes| {
+                    let body = std::str::from_utf8(&bytes)
+                        .map_err(|_| ())
+                        .and_then(|str| Value::from_str(str).map_err(|_| ()))
+                        .unwrap_or_else(|_| Value::from(base64::encode(bytes)));
+
+                    Some(body)
+                })
                 .or(warp::any().map(|| None))
                 .unify(),
         )
-        .map(|path: FullPath, headers: HeaderMap, body: Option<Value>| {
-            log::debug!("Path: {path:?}");
-            log::debug!("Headers:");
-            for header in headers.iter() {
-                log::debug!("    {header:?}");
-            }
-            if let Some(body) = body.as_ref() {
-                log::debug!("Body:");
-                log::debug!("{:#}", body);
-            }
-            let path = path.as_str();
-            let headers = headers
-                .into_iter()
-                .map(|x| format!("{x:?}"))
-                .collect::<Vec<_>>();
+        .map(
+            |method: Method,
+             path: FullPath,
+             headers: HeaderMap,
+             query: Value,
+             body: Option<Value>| {
+                log::debug!("Method: {method:?}");
+                log::debug!("Query:");
+                log::debug!("{:#}", query);
+                log::debug!("Path: {path:?}");
+                log::debug!("Headers:");
+                for header in headers.iter() {
+                    log::debug!("    {header:?}");
+                }
+                if let Some(body) = body.as_ref() {
+                    log::debug!("Body:");
+                    log::debug!("{:#}", body);
+                }
+                let path = path.as_str();
+                let headers = headers
+                    .into_iter()
+                    .map(|x| format!("{x:?}"))
+                    .collect::<Vec<_>>();
 
-            warp::reply::json(&(path, headers, body))
-        });
+                warp::reply::json(&(method.to_string(), path, headers, query, body))
+            },
+        );
 
     warp::serve(all)
         .run(SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), 8080))
